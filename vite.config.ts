ecb45855f749a098ee2b12a734dfb9cec59d0c0a
Johan <johanfausto47@gmail.com>
2025-01-21 18:33:30 -0600

    D

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Escucha en todas las interfaces
+    port: Number(process.env.PORT) || 3000, // Convierte PORT a número o usa 3000 como valor por defecto
+  },
+});
