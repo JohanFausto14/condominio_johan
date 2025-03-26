358fd90d5e7b840157792f7467a6570ec561a119
Johan <johanfausto47@gmail.com>
2025-03-26 12:05:04 -0600

    Rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
 import Admin from "./pages/Admin/admin";
 import User from "./pages/user/user";
@@ -11,11 +11,15 @@ import Permisos from "./pages/user/permisos";
 import MultasAd from "./pages/Admin/multas_ad";
 import PermisosAd from "./pages/Admin/permisos_ad";
 import PagosAd from "./pages/Admin/pagos_ad";
-import Olvido from "./pages/Olvido"; // Importa el componente Olvido
-import NuevaContraseña from "./pages/NuevaContraseña"; // Importa el componente NuevaContraseña
+import Olvido from "./pages/Olvido";
+import NuevaContraseña from "./pages/NuevaContraseña";
+import "./index.css";
 
-
-import "./index.css"; // Mantén el estilo global
+// Componente de protección simplificado
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const isAuthenticated = !!localStorage.getItem("token");
+  return isAuthenticated ? children : <Navigate to="/" replace />;
+};
 
 const rootElement = document.getElementById("root");
 
@@ -24,19 +28,87 @@ if (rootElement) {
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
+          {/* Rutas públicas */}
           <Route path="/" element={<Login />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/usuarios" element={<Usuarios />} />
-          <Route path="/pagos" element={<Pagos />} />
-          <Route path="/multas" element={<Multas />} />
-          <Route path="/permisos" element={<Permisos />} />
-          <Route path="/admin/multas_ad" element={<MultasAd />} />
-          <Route path="/admin/permisos_ad" element={<PermisosAd />} />
-          <Route path="/admin/pagos_ad" element={<PagosAd />} />
-          <Route path="/Olvido" element={<Olvido />} /> {/* Ruta para Olvido */}
-          <Route path="/restablecer/:token" element={<NuevaContraseña />} /> {/* Ruta para NuevaContraseña */}
+          <Route path="/Olvido" element={<Olvido />} />
+          <Route path="/restablecer/:token" element={<NuevaContraseña />} />
 
+          {/* Rutas protegidas */}
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/user"
+            element={
+              <ProtectedRoute>
+                <User />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/usuarios"
+            element={
+              <ProtectedRoute>
+                <Usuarios />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/pagos"
+            element={
+              <ProtectedRoute>
+                <Pagos />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/multas"
+            element={
+              <ProtectedRoute>
+                <Multas />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/permisos"
+            element={
+              <ProtectedRoute>
+                <Permisos />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/multas_ad"
+            element={
+              <ProtectedRoute>
+                <MultasAd />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/permisos_ad"
+            element={
+              <ProtectedRoute>
+                <PermisosAd />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/pagos_ad"
+            element={
+              <ProtectedRoute>
+                <PagosAd />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Ruta de fallback */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
